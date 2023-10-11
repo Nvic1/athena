1b0f6ff351dfea80e6f86494a80b477975eb447f
Oscar <135969090+Nvic1@users.noreply.github.com>
2023-10-11 00:33:04 +0000

    chore: Configure project structure

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+import {Button, ButtonGroup} from "@nextui-org/react";
+
+export default function Home() {
+  return (
+      <div className='font-bold text-4xl text-slate-700'>Main Layout Page
+      </div>
+    )
+}
